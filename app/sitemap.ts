f741b1acbbd8b624d897b2c2929ab3862ae77f49
JakubKomorowski <jakub.komorowski01@gmail.com>
2024-02-10 14:58:19 +0100

    add robots.txt file

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,6 +5,7 @@ import { Post } from "@/types/Post";
 import { MetadataRoute } from "next";
 
 export const revalidate = 3600;
+export const dynamic = "force-dynamic";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts: Post[] = await sanityFetch({
